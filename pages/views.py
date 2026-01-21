dd6aa9c73da60c0e314b04b16faa73ea044378a8
jeuanimo <jeuan.mitchell@gmail.com>
2026-01-20 21:29:58 -0600

    Initital Porfolio Code Push

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def  about_me_view(request):
+    return render(request, 'pages/about_me.html')
+def  experience_view(request):
+    return render(request, 'pages/experience.html')
+def  skills_view(request):
+    return render(request, 'pages/skills.html')
+
